8405a0c1ca98a9f6859c24c2df09e9dae0bed2d0
Wael Moughrbel <wmoughrbel@routesme.com>
2021-04-26 12:46:01 +0300

    Add valid audiences to new acceptable application Types

--- APIGateway/Models/AppSettings.cs
+++ APIGateway/Models/AppSettings.cs
@@ -12,5 +12,9 @@ namespace APIGateway.Models
         public string ValidAudience { get; set; }
         public string SessionTokenIssuer { get; set; }
         public string ValidIssuer { get; set; }
+        public string DashboardAudience { get; set; }
+        public string RoutesAppAudience { get; set; }
+        public string ScreenAudience { get; set; }
+        public string BusValidatorAudience { get; set; }
     }
 }

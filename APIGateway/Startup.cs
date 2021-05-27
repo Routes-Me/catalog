f66ad67c35ba7ef83e0c4655204b8b2fe68f5ff9
Wael Moughrbel <wmoughrbel@routesme.com>
2021-05-27 10:49:45 +0300

    change invitation audience in staging

--- APIGateway/Startup.cs
+++ APIGateway/Startup.cs
@@ -138,10 +138,9 @@ namespace APIGateway
                     ValidIssuer = appSettings.SessionTokenIssuer,
                     ValidateIssuerSigningKey = true,
                     IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(appSettings.InvitationSecretKey)),
-                    // verify signature to avoid tampering
-                    ValidateLifetime = true, // validate the expiration
+                    ValidateLifetime = true,
                     RequireExpirationTime = true,
-                    ClockSkew = TimeSpan.FromMinutes(5) // tolerance for the expiration date
+                    ClockSkew = TimeSpan.FromMinutes(5)
                 };
                 x.Events = jwtBearerEvents;
             });
